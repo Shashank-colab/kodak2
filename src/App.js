727ea66fb15abd190d19028851f95b1f59f0a1e2
Shashank-colab <85106840+Shashank-colab@users.noreply.github.com>
2022-03-22 12:57:36 +0530

    adding plane

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
 import { Canvas } from "react-three-fiber"
-
-import { ScrollControls, useScroll, Environment, OrbitControls, Stats, Scroll } from "@react-three/drei";
-
-import { Suspense } from "react"
+import {  ScrollControls, useScroll, Environment, Stats, Scroll } from "@react-three/drei";
+import { Suspense} from "react"
 import Plane from "./model";
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import Text from "./text.js";
 
-function Load_model() {
-  const gltf = useLoader(GLTFLoader, 'swamp_location/scene.gltf')
-  return (
-    <Suspense fallback={null}>
-      <primitive object={gltf.scene} />
-    </Suspense>
-  )
-}
+
+
+// function Load_model() {
+//   const ref = useRef()
+//   const scroll = useScroll()
+//   // const gltf = useLoader(GLTFLoader, 'swamp_location/scene.gltf')
+//   const gltf = useGLTF('/small-airplane-v3.gltf')
+//   useFrame(() => (console.log(ref), ref.current.position.z = scroll.offset * 120))
+//   return (
+//     <Suspense fallback={null}>
+//       <primitive object={gltf.scene} />
+//     </Suspense>
+//   )
+// }
 
 
 
@@ -23,34 +26,29 @@ function App() {
   const scroll = useScroll();
   return (
     <>
-      <Canvas style={{ height: 600 }} >
+      <Canvas style={{ height: 625 }} >
         <fog attach="fog" args={['#17171b', 30, 40]} />
         <color attach="background" args={['#17171b']} />
         <pointLight position={[5, 5, 5]} />
         <Suspense fallback={null}>
-          <ScrollControls pages={3} distance={2} >
-            <Scroll>
-              
-              <Plane rotation={[0, Math.PI * 1.25, 0]} position={[0,-2,0]}/>
-              <Plane rotation={[0, Math.PI * 1.25, 0]} position={[0,3,0]}/>
-              <Plane rotation={[0, Math.PI * 1.25, 0]} position={[0,-7,0]}/>
-              <Plane rotation={[0, Math.PI * 1.25, 0]} position={[0,-12,0]}/>
-              <Plane rotation={[0, Math.PI * 1.25, 0]} position={[0,-17,0]}/>
-              {/* <Load_model position={[0,-30,0]} /> */}
-            </Scroll>
-            
-            {/* <Load_model /> */}
+          <ScrollControls pages={5} distance={1} >
+
+            <group>
+              <Plane rotation={[0, Math.PI, 0]} position={[3, 0, 0]} />
+              <Plane rotation={[0, Math.PI, 0]} position={[3, 0, 32]} />
+              <Plane rotation={[0, Math.PI, 0]} position={[3, 0, 66]} />
+              <Plane rotation={[0, Math.PI, 0]} position={[3, 0, 96]} />
+            </group>
             <Environment preset="dawn" background />
             <Scroll html>
-              <h1 style={{ top: '100vh', color: 'Black',right:'100vh' }} >html in here (optional)</h1>
-              <h1 style={{ top: '100vh', color: 'Black' }}>second page</h1>
+              <Text />
             </Scroll>
           </ScrollControls>
         </Suspense>
         {/* <OrbitControls /> */}
         <Stats />
       </Canvas>
-      <button onClick={(e) => Load_model}>skip history</button>
+      {/* <button onClick={(e) => Load_model}>skip history</button> */}
     </>
   );
 }
--- src/model.js
+++ src/model.js
@@ -1,15 +1,20 @@
-import React from "react";
+
+import { React, useRef } from 'react';
+import {  useFrame } from '@react-three/fiber'
 import { useGLTF,useScroll } from "@react-three/drei";
 
 
 export default function Plane(props) {
+  const ref = useRef()
+  const scroll = useScroll()
   const { nodes, materials } = useGLTF("/small-airplane-v3.gltf");
+  useFrame(() => (ref.current.position.z = scroll.offset * 120))
   
   return (
     
     
-    <group {...props}>
-      <group scale={[0.5, 0.5, 0.5]}>
+    <group {...props} >
+      <group scale={[0.5, 0.5, 0.5]} ref={ref}>
         <mesh
           material={materials.White}
           geometry={nodes["buffer-0-mesh-0"].geometry}
